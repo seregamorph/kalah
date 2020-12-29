0088509f7cb9938e397cc0e9b7d9c52cff4fd708
seregamorph <serega.morph@gmail.com>
2020-12-29 12:14:34 +0300

    swagger doc

--- kalah-common/src/main/java/com/backbase/kalah/dto/GameDto.java
+++ kalah-common/src/main/java/com/backbase/kalah/dto/GameDto.java
@@ -1,5 +1,6 @@
 package com.backbase.kalah.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.net.URI;
@@ -8,7 +9,10 @@ import java.util.Map;
 @Data
 public class GameDto {
 
+    @Schema(description = "Game id")
     private long id;
+    @Schema(description = "Game self reference")
     private URI uri;
+    @Schema(description = "Game status, keys are [\"1\"..\"14\"], values are stones count for each pit")
     private Map<Integer, Integer> status;
 }
